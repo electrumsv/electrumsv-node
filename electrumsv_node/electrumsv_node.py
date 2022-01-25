7128d45aaa158d5c067f6736f91402ed9eb69e29
AustEcon <AustEcon0922@gmail.com>
2022-01-25 14:42:22 +1300

    Add -rpcallowip=0.0.0.0/0 & -rpcbind=0.0.0.0 to default configuration (#28)
    - This makes the node RPC and REST api reachable from host to docker

--- electrumsv_node/electrumsv_node.py
+++ electrumsv_node/electrumsv_node.py
@@ -101,7 +101,9 @@ def shell_command(config_path: Optional[str] = None, data_path: Optional[str] = 
         f"-preload=1",
         f"-rejectmempoolrequest=0",
         f"-whitelist=127.0.0.1",
-        f"-debug=1"
+        f"-debug=1",
+        f"-rpcallowip=0.0.0.0/0",
+        f"-rpcbind=0.0.0.0"
     ])
     if extra_params is not None:
         split_command.extend(extra_params)
